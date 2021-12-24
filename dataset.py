011683b2387aaeb4b0a0ff3959f447f874b03174
TaekyungKi <1411remember@naver.com>
2021-12-24 09:37:22 +0000

    feat: dataset class

--- dataset.py
+++ dataset.py
@@ -6,17 +6,24 @@ from torch.utils.data import Dataset, DataLoader
 class ClassifierDataset(Dataset):
     def __init__(self, json_path):
         self.json_path = json_path
+        with open(json_path, 'r', encoding = 'UTF-8-sig') as j:
+            self.mwp_cpae = json.load(j)
+        
+    def read_cpae(self, idx):
+        c = self.mwp_cpae[str(idx)]['class']
+        p = self.mwp_cpae[str(idx)]['question']
+        a = self.mwp_cpae[str(idx)]['answer']
+        e = self.mwp_cpae[str(idx)]['equation'] if c == '산술연산' else 'none'
+        return c, p, a, e
 
     def __len__(self):
-        return 0
+        return len(self.mwp_pae.keys())
 
     def __getitem__(self, idx):
-        return 0
+        return self.read_cpae(idx)
 
 
 if __name__ == '__main__':
     json_path = './public_mwp_data.json'
 
     dataset = ClassifierDataset(json_path = json_path)
-    dataloader = DataLoader(dataset, batch_size = 4, shuffle = True, pin_memort = True)
-    print(dataset.__len__())
